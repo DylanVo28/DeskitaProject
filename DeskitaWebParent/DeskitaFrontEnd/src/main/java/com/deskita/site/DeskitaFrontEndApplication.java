d0b22264f46e73b474040a3954b643b15b73a8f2
DinhVo <dinh.vo@mediastep.com>
2021-08-20 23:36:55 +0700

    First commit

---
+++ DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/site/DeskitaFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.deskita.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskitaFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskitaFrontEndApplication.class, args);
+	}
+
+}
