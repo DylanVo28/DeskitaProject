6ccffba742c0c4252e132881f0477fc8a6595357
DinhVo <dinh.vo@mediastep.com>
2021-09-27 16:49:13 +0700

    code product on front end

--- DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/service/ProductService.java
+++ DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.deskita.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,13 +9,24 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.deskita.common.entity.Product;
+import com.deskita.common.entity.ProductDetail;
+import com.deskita.common.entity.ProductImage;
+import com.deskita.repository.ProductDetailRepository;
+import com.deskita.repository.ProductImageRepository;
 import com.deskita.repository.ProductRepository;
 
 @Service
 public class ProductService {
-	public static int PAGE_SIZE=10;
+	public static int PAGE_SIZE=6;
+	
 	@Autowired
 	private ProductRepository repo;
+	
+	@Autowired
+	private ProductDetailRepository pdr;
+	
+	@Autowired
+	private ProductImageRepository pir;
 	
 	public Page<Product> pagingProduct(int currentPage){
 		Pageable pageable=PageRequest.of(currentPage-1, PAGE_SIZE);
@@ -21,4 +34,19 @@ public class ProductService {
 		return page;
 	}	
 
+	public List<Product> listAll(){
+		return (List<Product>) repo.findAll();
+	}
+	
+	public Product findProductById(int id) {
+		return repo.findById(id).get();
+	}
+	
+	public List<ProductDetail> getAllProductDetails(int id){
+		return pdr.getProductDetailsByProductId(id);
+	}
+	
+	public List<ProductImage> getAllProductImages(int id){
+		return pir.findImageByProductId(id);
+	}
 }
