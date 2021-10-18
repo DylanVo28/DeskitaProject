80fdf5c295ad4ba8883e055d57cfdf64f33cf6f3
DinhVo <dinh.vo@mediastep.com>
2021-10-18 14:33:05 +0700

    code order customer

--- DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/controller/OrderController.java
+++ DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/controller/OrderController.java
@@ -1,15 +1,39 @@
 package com.deskita.controller;
 
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.deskita.common.entity.Customer;
+import com.deskita.common.entity.Order;
+import com.deskita.common.entity.Product;
 import com.deskita.common.entity.ProductDetail;
+import com.deskita.common.entity.type.OrderStatus;
+import com.deskita.common.entity.type.PaymentMethod;
 import com.deskita.dto.OrderDTO;
+import com.deskita.security.CustomerAuthentication;
+import com.deskita.security.DeskitaCustomerDetails;
+import com.deskita.service.CustomerService;
+import com.deskita.service.OrderService;
 
 @Controller
 public class OrderController {
 
+	@Autowired
+	private CustomerService customerService;
+
+	@Autowired
+	private OrderService orderService;
+	
 	@PostMapping("/order-now")
 	public String viewOrder(
 			@RequestParam(name="quantity_product",required = false) Integer quantityProduct,
@@ -18,10 +42,47 @@ public class OrderController {
 			@RequestParam(name="phone_number",required = false) String phoneNumber,
 			@RequestParam(name="payment_method",required = false) String paymentMethod,
 			@RequestParam(name="total_price_order",required = false) String totalPriceOrder,
-			OrderDTO orderDto
+			@RequestParam(name="shipping_price",required = false) String shippingPrice,
+			@RequestParam(name="product_id",required = false) Integer productId,
+			@RequestParam(name="product_detail_id",required=false) Integer productDetailId,
 			
+			@AuthenticationPrincipal DeskitaCustomerDetails loggedUser,
+			Model model
 			) {
-		System.out.println(orderDto.toString());
-		return "redirect:/";
+		
+		
+		PaymentMethod paymentMethodOrder= PaymentMethod.valueOf(paymentMethod);
+
+		OrderDTO orderDTO=new OrderDTO(quantityProduct, fullname, address, phoneNumber, paymentMethod, Float.parseFloat(totalPriceOrder),Float.parseFloat(shippingPrice),productId,productDetailId);
+	
+		Customer customer=customerService.getCustomerByEmail(loggedUser.getUsername()) ;
+
+		Order order=orderService.createOrder(customer, paymentMethodOrder, orderDTO);
+		model.addAttribute("order",order);
+		
+		return "order/order_complete";
+	}
+	
+	@GetMapping("/code-ui")
+	public String viewCodeUI() {
+		return "order/order_detail";
+	}
+	
+	@GetMapping("/my-order")
+	public String myOrder(@AuthenticationPrincipal DeskitaCustomerDetails loggedUser,Model model) {
+		Customer customer=customerService.getCustomerByEmail(loggedUser.getUsername()) ;
+		Set<Order> orders=customer.getOrders();
+		model.addAttribute("orders",orders);
+//		System.out.println(customer.getOrders());
+		return "order/my_order";
+	}
+	
+	@GetMapping("/order/{orderId}")
+	public String orderDetail(@AuthenticationPrincipal DeskitaCustomerDetails loggedUser,Model model,@PathVariable(name = "orderId") Integer id) {
+		Order order=orderService.findOrderById(id);
+		Customer customer=customerService.getCustomerByEmail(loggedUser.getUsername()) ;
+		model.addAttribute("order",order);
+		model.addAttribute("customer",customer);
+		return "order/order_detail";
 	}
 }
--- DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/dto/OrderDTO.java
+++ DeskitaWebParent/DeskitaFrontEnd/src/main/java/com/deskita/dto/OrderDTO.java
@@ -9,47 +9,123 @@ import com.deskita.common.entity.ProductDetail;
 
 public class OrderDTO {
 
+	private int quantityProduct;
 	
-	ProductDetail productDetail;
+	private String fullName;
+	
+	private String address;
+	
+	private String phoneNumber;
+	
+	private String paymentMethod;
+	
+	private float totalPriceOrder;
 
-	Product product;
+	private float shippingPrice;
+	
+	private int productId;
+	
+	private int productDetailId;
+	
+	public int getQuantityProduct() {
+		return quantityProduct;
+	}
 
-	public OrderDTO(ProductDetail productDetail, Product product) {
+	public void setQuantityProduct(int quantityProduct) {
+		this.quantityProduct = quantityProduct;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public float getTotalPriceOrder() {
+		return totalPriceOrder;
+	}
+
+	public void setTotalPriceOrder(float totalPriceOrder) {
+		this.totalPriceOrder = totalPriceOrder;
+	}
+
+	public float getShippingPrice() {
+		return shippingPrice;
+	}
+
+	public void setShippingPrice(float shippingPrice) {
+		this.shippingPrice = shippingPrice;
+	}
+	
+	
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getProductDetailId() {
+		return productDetailId;
+	}
+
+	public void setProductDetailId(int productDetailId) {
+		this.productDetailId = productDetailId;
+	}
+
+	public OrderDTO(int quantityProduct, String fullName, String address, String phoneNumber, String paymentMethod,
+			float totalPriceOrder,float shippingPrice,int productId,int productetailId) {
 		super();
-		this.productDetail = productDetail;
-		this.product = product;
+		this.quantityProduct = quantityProduct;
+		this.fullName = fullName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.paymentMethod = paymentMethod;
+		this.totalPriceOrder = totalPriceOrder;
+		this.shippingPrice=shippingPrice;
+		this.productId=productId;
+		this.productDetailId=productetailId;
+		
 	}
-
-	
-	
-	public ProductDetail getProductDetail() {
-		return productDetail;
-	}
-
-
-
-	public void setProductDetail(ProductDetail productDetail) {
-		this.productDetail = productDetail;
-	}
-
-
-
-	public Product getProduct() {
-		return product;
-	}
-
-
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-
 
 	@Override
 	public String toString() {
-		return "OrderDTO [productDetail=" + productDetail + ", product=" + product + "]";
+		return "OrderDTO [quantityProduct=" + quantityProduct + ", fullName=" + fullName + ", address=" + address
+				+ ", phoneNumber=" + phoneNumber + ", paymentMethod=" + paymentMethod + ", totalPriceOrder="
+				+ totalPriceOrder + ", shippingPrice=" + shippingPrice + ", productId=" + productId
+				+ ", productDetailId=" + productDetailId + "]";
 	}
+	
+	
+	
 	
 	
 }
